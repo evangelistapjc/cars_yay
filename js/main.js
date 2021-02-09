9670b4f95c2f862ea5c107c8442eb7b017ed18af
PAUL JOHN EVANGELISTA <evangelistapjc@gmail.com>
2021-02-08 16:20:13 -0800

    Jorell's edits, main photo intro

---
+++ js/main.js
@@ -0,0 +1,33 @@
+scrollNav = () => {                              //Hides and shows navbar
+    let nav = document.querySelector(".navbar");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        nav.style.margin = 0;
+        nav.style.width = "100%";
+        hideMain();
+    } else {
+        nav.style.margin = "100px";
+        nav.style.width = "80%";
+        showMain();
+    }
+}
+
+hideMain = () => {                                //Hides button and logo
+    let logo = document.getElementById("logo");
+    let button = logo.nextElementSibling;
+    logo.style.maxHeight = 0;
+    logo.style.margin = -80 + "px";
+    button.style.maxHeight = 0;
+    button.style.margin = -80 + "px";
+
+}
+
+showMain = () => {                                //Shows Button and logo
+    let logo = document.getElementById("logo");
+    let button = logo.nextElementSibling;
+    logo.style.maxHeight = "500px"
+    logo.style.margin = 50 + "px";
+    button.style.maxHeight = "500px"
+    button.style.margin = 200 + "px";
+}
+
+window.onscroll = ()=>{scrollNav()};
